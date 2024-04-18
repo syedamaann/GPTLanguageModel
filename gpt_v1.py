824c643d4afb555161133817909dd725277f3b60
syedamaann <sydamaannnn@gmail.com>
2024-04-18 14:53:28 +0530

    add residual block, val loss 2.08

--- gpt_v1.py
+++ gpt_v1.py
@@ -89,9 +89,12 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
 
 # feedforward layer
 class FeedFoward(nn.Module):
@@ -100,13 +103,30 @@ class FeedFoward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
         )
 
     def forward(self, x):
         return self.net(x)
 
+# transformer block (communication followed by computation)
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
+        return x
+      
 # model (very simple bigram language model)
 class BigramLanguageModel(nn.Module):
   
@@ -114,8 +134,11 @@ class BigramLanguageModel(nn.Module):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)       # token embeddings
     self.position_embedding_table = nn.Embedding(block_size, n_embd)    # positional embeddings
-    self.sa_heads = MultiHeadAttention(4, n_embd//4)                    # multi-head self-attention (4 heads of size n_embd//4)
-    self.ffwd = FeedFoward(n_embd)
+    self.blocks = nn.Sequential(
+      Block(n_embd, n_head=4),
+      Block(n_embd, n_head=4),
+      Block(n_embd, n_head=4),
+    )
     self.lm_head = nn.Linear(n_embd, vocab_size)                        # linear layer
     
   def forward(self, idx, target=None):
@@ -124,8 +147,7 @@ class BigramLanguageModel(nn.Module):
     token_emb = self.token_embedding_table(idx)  # (B,T,C)
     pos_embd = self.position_embedding_table(torch.arange(T))  # (T,C)
     x = token_emb + pos_embd    # (B,T,C)
-    x = self.sa_heads(x)        # (B,T,C)
-    x = self.ffwd(x)            # (B,T,C)
+    x = self.blocks(x)          # (B,T,C)
     logits = self.lm_head(x)    # (B,T, vocab_size)
     
     if target is None:
@@ -160,12 +182,12 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 for iter in range(max_iters):
   x, y = get_batch('train')   # sample a batch of data
   logits, loss = model(x, y)  # forward pass
-  optimizer.zero_grad()       # zero out the gradients
+  optimizer.zero_grad(set_to_none=True)       # zero out the gradients
   loss.backward()             # backward pass
   optimizer.step()            # update the weights
   
   # print loss on train and val sets after every few iterations
-  if iter % eval_interval == 0:
+  if iter % eval_interval == 0 or iter == max_iters - 1:
     losses = estimate_loss()
     print(f'Iter: {iter}, Train loss: {losses["train"]}, Val loss: {losses["val"]}')
 
